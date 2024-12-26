f40629a74e87754590cf146a9bd935230d074181
Yeshaya <andyshapiro613@gmail.com>
2024-12-26 13:13:36 -0500

    new mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -114,14 +114,7 @@ const Home: React.FC = () => {
               {features.map((feature, index) => (
                 <motion.div
                   key={index}
-                  initial={{ opacity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.4, delay: index * 0.1 }}
-                  whileHover={{ 
-                    scale: 1.05,
-                    transition: { duration: 0.2 }
-                  }}
-                  viewport={{ once: true }}
+                  whileHover={{ y: -10 }}
                   className="bg-black/30 p-8 rounded-lg border border-white/5 hover:border-orangePulse/30 transition-all duration-300"
                 >
                   <motion.div 
